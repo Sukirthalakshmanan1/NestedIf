fbac406d74ebe81019f4c70fea5d904d9f55ebd1
Sukirthalakshmanan <sukirtha3700@gmail.com>
2022-08-19 10:04:59 +0530

    Changes done

--- NestedIf/Program.cs
+++ NestedIf/Program.cs
@@ -10,7 +10,7 @@ namespace NestedIf
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
+            Console.WriteLine("Hello pgm");
             Console.ReadKey();
         }
     }
