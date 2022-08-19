1b56a0c8646f10016acd2e720bae5cf041a14600
Sukirthalakshmanan <sukirtha3700@gmail.com>
2022-08-19 10:22:47 +0530

    Added code

--- NestedIf/Program.cs
+++ NestedIf/Program.cs
@@ -11,7 +11,23 @@ namespace NestedIf
         static void Main(string[] args)
         {
             Console.WriteLine("Hello pgm");
+
+            int n;
+            Console.WriteLine("Enter n: ");
+            n=Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < n ; i++)
+            {
+                if (i >= 0)
+                {
+                    Console.WriteLine("Number is "+i);
+                }
+                else
+                {
+                    Console.WriteLine("less than 0");
+                }
+            }
             Console.ReadKey();
+
         }
     }
 }
