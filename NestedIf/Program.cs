5ba0ff310b9fefbc385edce14146b651b0ad4a98
Sukirthalakshmanan <sukirtha3700@gmail.com>
2022-08-19 10:28:57 +0530

    created a Branching pgm if else

--- NestedIf/Program.cs
+++ NestedIf/Program.cs
@@ -11,6 +11,25 @@ namespace NestedIf
         static void Main(string[] args)
         {
             Console.WriteLine("Open my repo");
+            int n;
+            Console.WriteLine("Enter n: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            if (n != 0)
+            {
+                if((n>0) && (n < 10))
+                {
+                    Console.WriteLine("n is from 0 to 9");
+                }
+                else if(n==10){
+                    Console.WriteLine("n is equal to 10");
+
+                }
+                else
+                {
+                    Console.WriteLine("n is greater than 10");
+                }
+            }
+
         }
     }
 }
